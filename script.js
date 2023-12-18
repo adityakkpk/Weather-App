ddff7ea70b207be9877ca846967b4940dd81a4a6
Aditya <akkpk933@gmail.com>
2023-12-18 19:12:33 +0530

    Adding feature user location

--- script.js
+++ script.js
@@ -7,18 +7,42 @@ const para = document.querySelector('p');
 
 para.innerText = '';
 
+//callbacks for navigators
+async function gotLocation (position) {
+  //Displaying weather using Longitude and latitude
+  const res = await getDataByLongLat(position.coords.latitude, position.coords.longitude);
+  input.value = res.location.name;
+  cityName.innerHTML = `City: <span>${res.location.name}, ${res.location.region} - ${res.location.country}</span>`
+  cityTime.innerHTML = `Localtime : <span>${res.location.localtime}</span>`
+  cityTemp.innerHTML = `Temperature : <span>${res.current.temp_c}<sup>&deg;</sup>C </span>`
+}
+function failedToGet () {
+ console.log('Some Error Occured.');
+}
 window.addEventListener('load', ()=>{
-  renderWeather();
+  //asking user location 
+  navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
+  renderCityWeather();
 });
 
-async function getData(cityName) {
+//Geting location by Latitude and Longitude
+async function getDataByLongLat(lat,long) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=7ff49be441184e749d853351231712&q=${lat},${long}&aqi=yes`
+    );
+    return await response.json();
+}
+  
+//getting location by City name
+async function getDataByCity(cityName) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=7ff49be441184e749d853351231712&q=${cityName}&aqi=yes`
   );
   return await response.json();
 }
 
-async function renderWeather() {
+//Displaying weather using City name
+async function renderCityWeather() {
   const value = input.value;
   if(!value){
     para.innerText = "Please Enter a City Name";
@@ -26,19 +50,19 @@ async function renderWeather() {
   setTimeout(() => {
     para.innerText = '';
   }, 1000);
-  const result = await getData(value);
+  const result = await getDataByCity(value);
   cityName.innerHTML = `City: <span>${result.location.name}, ${result.location.region} - ${result.location.country}</span>`
   cityTime.innerHTML = `Localtime : <span>${result.location.localtime}</span>`
   cityTemp.innerHTML = `Temperature : <span>${result.current.temp_c}<sup>&deg;</sup>C </span>`
 }
 
 btn.addEventListener('click', () => {
-  renderWeather();
+  renderCityWeather();
 });
 
 function handleInputKeydown(event) {
 
   if (event.key === 'Enter') {
-      renderWeather();
+      renderCityWeather();
   }
 }
